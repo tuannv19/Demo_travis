caf06b0471e3242e89a04ab3175838f9e25ee29e
tuannv19 <tuan20070904@gmail.com>
2018-07-02 16:10:16 +0700

    Remove log

--- Demo_travis/Demo_travis/AppDelegate.swift
+++ Demo_travis/Demo_travis/AppDelegate.swift
@@ -30,7 +30,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         print("Hello")
         print("Hello")
         print("Hello")
-        print("Hello")
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
