a96ee895097051b5dcd70dfd97c5c54f8ed56a2e
tuannv19 <tuan20070904@gmail.com>
2018-07-02 15:11:26 +0700

    Morelog

--- Demo_travis/Demo_travis/AppDelegate.swift
+++ Demo_travis/Demo_travis/AppDelegate.swift
@@ -29,6 +29,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         print("Hello")
         print("Hello")
         print("Hello")
+        print("Hello")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
