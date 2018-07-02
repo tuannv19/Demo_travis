f78f531ca31ee7659c4f674dbf37d32e1dcc9f97
tuannv19 <tuan20070904@gmail.com>
2018-07-02 14:43:48 +0700

    aaa

---
+++ Demo_travis/Demo_travis/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Demo_travis
+//
+//  Created by tuannv on 7/2/18.
+//  Copyright © 2018 tuannv. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
